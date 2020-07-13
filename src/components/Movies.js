d813547d06bcf0cd647eed64c45b7330bd180e7b
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-07-13 22:21:59 +0200

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const moviesList = movies.map(movie =>
+    <div>
+      Title: {movie.title}
+      Time: {movie.time}
+      <ul>
+        {movie.genres.map(genre=> <li>{genre}</li>)}
+      </ul>
+    </div>
+  )
   return (
     <div>
-        {/*{code here}*/}
+        <h1>
+          Movies Page
+        </h1>
+        {moviesList}
     </div>
-  );
+  )
 };
 
 export default Movies;
