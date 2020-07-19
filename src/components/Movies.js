109e629b5eed9c5daad10661c2a913b6e44393e7
jayson_kesterson <your_email@example.com>
2020-07-19 13:34:51 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const moviesList = movies.map(movie =>
+    <div>
+      Title: {movie.title}
+      <br></br>
+      Time: {movie.time}
+      <ul>
+        {movie.genres.map(genre=> <li>{genre}</li>)}
+      </ul>
+    </div>
+  )
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {moviesList}
     </div>
   );
 };
