1481ce5d11eb4a01fd9257ed968af66752d3567f
Jack <jackrrye@gmail.com>
2020-07-23 18:26:05 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
+const Movies = (props) => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {/* The <div> should contain the movie's title, time and a <ul> with a list of its genres. */}
+      {movies.map(movie => 
+        <div>
+          <p>{movie.title}</p>
+          <p>{movie.time}</p>
+          <ul>
+            {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+        </div>)}
     </div>
   );
 };
